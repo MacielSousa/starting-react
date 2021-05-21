690289367bf282a8ec946acec215fb391355be0d
Macielsousa <fmacielsousa@gmail.com>
2021-05-21 20:21:21 -0300

    webpack_babel

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+    return React.createElement(
+        'a',
+        {href: 'https://github.com/macielsousa'},
+        'Git',
+    )
+};
+
+export default App;
