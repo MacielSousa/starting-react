941a466713827b5f62420802d52638b5ac70bc8e
Maciel Sousa <fmacielsousa@gmail.com>
2021-05-23 00:52:23 -0300

    Fragmento

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const App = () => {
-    return <div>Meu App</div>
+    return (
+        <React.Fragment>
+            <label htmlFor="nome">Nome</label>
+            <input type="text" id="nome"></input>
+        </React.Fragment>
+    );
 }
 
 export default App;
