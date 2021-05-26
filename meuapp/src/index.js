14bfabb5e95a53d612be073752b7c84bf20204dc
Maciel Sousa <fmacielsousa@gmail.com>
2021-05-25 23:51:02 -0300

    Desafio Produto

--- meuapp/src/index.js
+++ meuapp/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Persona from './Persona';
 import Array from './Array'
+import Produtos from './Produtos'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Array />
+    <Produtos />
   </React.StrictMode>,
   document.getElementById('root')
 );
