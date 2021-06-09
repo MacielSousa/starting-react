9928260936b53fa74f123f6ad843e8c951bf5bb8
Maciel Sousa <fmacielsousa@gmail.com>
2021-06-08 22:48:53 -0300

    Comentarios useRef

--- meuapp/src/Hooks/Hooks useRef/UseRef.js
+++ meuapp/src/Hooks/Hooks useRef/UseRef.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 const UseRef = () => {
-    const video = React.useRef();
-    console.log(video.current.currentTime)
-    return <video ref={video}/>;
+    const [comentorios, setComentarios] = React.useState([]);
+    const [input, setInput] = React.useState('teste input');
+    const inputElement = React.useRef();
+    
+   function handleClick(){
+       setComentarios([...comentorios, input]);
+       setInput('');
+       inputElement.current.focus();
+   }
+    
+    return <div>
+        <ul>
+            {comentorios.map((comentario, index) => (
+                <li key={index}>{comentario}</li>
+            ))}
+            
+        </ul>
+        <input 
+            type="text"
+            ref={inputElement}
+            value={input}
+            onChange={({target}) => setInput(target.value)}
+        />
+        <br />
+        <button onClick={handleClick}>Enviar</button>
+    </div>
 }
 
 export default UseRef;
