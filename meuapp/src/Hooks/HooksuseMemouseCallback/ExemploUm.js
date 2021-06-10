8a2b20135daa356028f51f07d949677cea1f8b4e
Maciel Sousa <fmacielsousa@gmail.com>
2021-06-10 15:38:11 -0300

    Aumentando a performance da aplicação com useMemo

--- meuapp/src/Hooks/HooksuseMemouseCallback/ExemploUm.js
+++ meuapp/src/Hooks/HooksuseMemouseCallback/ExemploUm.js
@@ -1,14 +1,26 @@
 import React, { memo } from 'react';
 
+function opercaoLenta() {
+    let c;
+    for(let i = 0; i < 100000000; i++){
+        c = i + i / 10;
+    }
+    return c;
+}
 const ExemploUm  = () => {
     const [contar, setContar] = React.useState(0);
 
-    const valor = React.useMemo(() => {
-        const localItem = window.localStorage.getItem('produto');
-        console.log('Aconteceu o memo');
-        return localItem
-    }, [])
+    /** Exemplo que deixa o desenpenho em baixa performance
+         const valor = React.useMemo(() => {
+            const localItem = window.localStorage.getItem('produto');
+            console.log('Aconteceu o memo');
+            return localItem
+        }, []) 
+     */
+    const t1 = performance.now();
+    const valor = React.useMemo(() => opercaoLenta(), []);
     console.log(valor);
+    console.log(performance.now() - t1)
 
     return <button onClick={() => setContar(contar + 1)}>{contar}</button>
 };
