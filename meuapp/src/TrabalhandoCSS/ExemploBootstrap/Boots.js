1ec27b5378737a4f8c4b3414357383a6e326603b
macielsousa <fmacielsousa@gmail.com>
2021-06-27 03:37:11 -0300

    example of use react-bootstrap

--- meuapp/src/TrabalhandoCSS/ExemploBootstrap/Boots.js
+++ meuapp/src/TrabalhandoCSS/ExemploBootstrap/Boots.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card'
 const Boots = () => {
-    return <div className="card bg-dark text-white m-5" style={{ maxWidth: '18rem' }}>
-        <div className="card-header">Notebook</div>
-        <div className="card-body">
-            <h5 className="card-title">R$ 2500</h5>
-            <p>
-                Esse e um Notebook com 1tb, 16GB de ram e placa de video de 16GB.
-            </p>
-        </div>
-    </div>
+    return <Card bg="dark" className=" text-white m-5" style={{ maxWidth: '18rem' }}>
+        <Card.Header>Notebook</Card.Header>
+        <Card.Body>
+            <Card.Title>R$ 2500</Card.Title>
+            <Card.Text>
+                Esse é um notebook com 1tb, 16GB de ram e placa de vídeo de 16GB.
+            </Card.Text>
+        </Card.Body>
+    </Card>
 }
 
 export default Boots;
